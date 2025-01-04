a05c592f24c78904be12c0e8ac276c39a952580d
Gowtham sree charan reddy <115145278+gowthamrdyy@users.noreply.github.com>
2025-01-04 10:19:34 +0530

    Update CommentSection.tsx

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -4,40 +4,40 @@ import { useState, useEffect } from "react";
 
 const comments = [
     {
-        name: "Rachit Kumar Pandey",
+        name: "Prasanna kumar reddy",
         comment: "Having worked with him, I can confidently say he delivers nothing short of perfection. Always striving to improve, his work speaks volumes for itself—no words needed.",
-        title: "Student at IIT (ISM)",
+        title: "Student at SRM",
         image: "/testimonials/rachit.jpg"
     },
     {
-        name: "Shashvat Aghera",
+        name: "Guna adhityya reddy",
         comment: "All his work is very minimalist, and refined, that at this point, his solutions to problems are not only convenient, but also a treat for the eyes. He crafts marvellous programs and websites.",
         title: "2nd year at SRM University",
-        image: "https://cdn.sanity.io/images/4orhaocq/production/8885b7c77a8a32a1eaec3a75ea6910fa81355ed9-2250x3000.jpg"
+        image: "/testimonials/adi.jpg"
     },
     {
-        name: "Yogesh",
+        name: "Dheeraj",
         comment: "Great UI and UX design skills and an experienced front-end developer. Inspired others in front-end development because of his exemplary work.",
         title: "Founder at Neugence",
-        image: "/testimonials/yogesh.png"
+        image: "/testimonials/Dheeraj.jpg"
     },
     {
-        name: "Keshava raj",
+        name: "Manohar",
         comment: "His work on this project was outstanding. His creativity, dedication, and attention to detail were evident in every aspect, resulting in exceptional outcomes. A true testament to his talent and hard work.",
         title: "Student at SRM University",
-        image: "/testimonials/keshav.jpg"
+        image: "/testimonials/Manohar.jpg"
     },
     {
-        name: "Ishaan Khurana",
+        name: "Rahul",
         comment: "One of the most talented developers I've ever met, he can create amazing designs and has the ability to make them a reality. The things he has made impacts thousands on a daily basis and i believe that any team would be lucky to have an asset like him",
         title: "Syndicate at NextTech Lab",
-        image: "/testimonials/ishaan.png"
+        image: "/testimonials/Rahul.jpg"
     },
     {
-        name: "Abadima",
+        name: "Tharun",
         comment: "He's an amazing developer and a great person to work with. I've worked with him for over two years on projects like simply-djs and simply-xp. He's talented, reliable, and great at problem-solving, making teamwork fun and productive.",
         title: "Contributor to simply-xp",
-        image: "/testimonials/abadima.jpeg"
+        image: "/testimonials/Tharun.jpg"
     }
 ];
 
